c6d61cc142848aa765497a839c43c853de5d6e95
nalyD <dylanmarin2018@gmail.com>
2024-08-26 14:20:48 -0600

    added feature for highlighting the changed word. fcc tests pass

--- components/translator.js
+++ components/translator.js
@@ -6,17 +6,21 @@ const britishOnly = require("./british-only.js");
 const preserveCase = (string, regex, replacement) => {
   return string.replace(regex, (match) => {
     if (match[0] === replacement[0].toUpperCase()) {
-      return replacement.charAt(0).toUpperCase() + replacement.slice(1);
+      return highlight(replacement.charAt(0).toUpperCase() + replacement.slice(1));
     } else {
-      return replacement;
+      return highlight(replacement);
     }
   });
 };
 
+const highlight = (word) => {
+    return `<span class="highlight">${word}</span>`
+}
+
 class Translator {
   americanToBritish(testString) {
     let translatedString = testString.replace(/([01]?[0-9]|2[0-3]):[0-5][0-9]/g, (match) => {
-        return match.replace(':', '.');
+        return highlight(match.replace(':', '.'));
     });
 
     for (const [american, british] of Object.entries(americanOnly)) {
@@ -39,7 +43,7 @@ class Translator {
   }
   britishToAmerican(testString) {
     let translatedString = testString.replace(/([01]?[0-9]|2[0-3]).[0-5][0-9]/g, (match) => {
-        return match.replace('.', ':');
+        return highlight(match.replace('.', ':'));
     });
 
     for (const [british, american] of Object.entries(britishOnly)) {
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,36 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (text === undefined || !locale) {
+      return res.json({ error: "Required field(s) missing" });
+    } else if (text === "") {
+      return res.json({ error: "No text to translate" });
+    } else if (
+      locale !== "american-to-british" &&
+      locale !== "british-to-american"
+    ) {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    let translation;
+    if (locale === "american-to-british") {
+      translation = translator.americanToBritish(text);
+    }
+
+    if (locale === "british-to-american") {
+      translation = translator.britishToAmerican(text);
+    }
+    return res.json({
+      text,
+      translation:
+        translation === text ? "Everything looks good to me!" : translation,
     });
+  });
 };
